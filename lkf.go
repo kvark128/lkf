b755a3628303d91396c547b09e211b7ae7148c03
Kvark <kvark128@yandex.ru>
2020-09-05 04:04:21 +0800

    Cosmetic changes

--- lkf.go
+++ lkf.go
@@ -1,9 +1,9 @@
 package lkf
 
 const (
-	blockSize        = 128 // The block size in words. Each word is uint32
-	BlockSizeInBytes = blockSize * 4
-	delta            = 0x9e3779b9
+	BlockSize        = 512           // The block size in bytes
+	blockSizeInWords = BlockSize / 4 // The block size in words. Each word is uint32
+	delta            = 0x9e3779b9    // Magic constant
 )
 
 // The 128-bit key for encrypting/decrypting lkf files. It is divided into 4 parts of 32 bit each.
@@ -14,15 +14,15 @@ var key = [4]uint32{
 	0x05d47c66,
 }
 
-func calcKey(leftWord, rightWord, r, k uint32) uint32 {
-	n := (leftWord>>5 ^ rightWord<<2) + (rightWord>>3 ^ leftWord<<4)
-	n ^= (key[(r>>2^k)&3] ^ leftWord) + (r ^ rightWord)
+func calcKey(lWord, rWord, x, k uint32) uint32 {
+	n := (lWord>>5 ^ rWord<<2) + (rWord>>3 ^ lWord<<4)
+	n ^= (key[(x>>2^k)&3] ^ lWord) + (x ^ rWord)
 	return n
 }
 
 // A Cryptor represents internal buffer, used for encrypting/decrypting passed data.
 type Cryptor struct {
-	block [blockSize]uint32
+	block [blockSizeInWords]uint32
 }
 
 func (c *Cryptor) toBlock(data []byte) {
@@ -42,44 +42,44 @@ func (c *Cryptor) fromBlock(data []byte) {
 	}
 }
 
-// Encrypt encrypts first len(data) / BlockSizeInBytes blocks. Returns the size all encrypted blocks. If len(data) < BlockSizeInBytes - returns 0.
+// Encrypt encrypts first len(data) / BlockSize blocks. Returns the size all encrypted blocks. If len(data) < BlockSize - returns 0.
 func (c *Cryptor) Encrypt(data []byte) int {
-	nBlocks := len(data) / BlockSizeInBytes
+	nBlocks := len(data) / BlockSize
 	for i := 0; i < nBlocks; i++ {
-		b := data[i*BlockSizeInBytes:]
+		b := data[i*BlockSize:]
 		c.toBlock(b)
 		// Used only 3 rounds
 		for r := uint32(1); r <= 3; r++ {
 			var x uint32 = r * delta
-			lWord := c.block[blockSize-1]
-			for k := 0; k < blockSize-1; k++ {
+			lWord := c.block[blockSizeInWords-1]
+			for k := 0; k < blockSizeInWords-1; k++ {
 				c.block[k] += calcKey(lWord, c.block[k+1], x, uint32(k))
 				lWord = c.block[k]
 			}
-			c.block[blockSize-1] += calcKey(lWord, c.block[0], x, uint32(blockSize-1))
+			c.block[blockSizeInWords-1] += calcKey(lWord, c.block[0], x, uint32(blockSizeInWords-1))
 		}
 		c.fromBlock(b)
 	}
-	return nBlocks * BlockSizeInBytes
+	return nBlocks * BlockSize
 }
 
-// Decrypt decrypts first len(data) / BlockSizeInBytes blocks. Returns the size all decrypted blocks. If len(data) < BlockSizeInBytes - returns 0.
+// Decrypt decrypts first len(data) / BlockSize blocks. Returns the size all decrypted blocks. If len(data) < BlockSize - returns 0.
 func (c *Cryptor) Decrypt(data []byte) int {
-	nBlocks := len(data) / BlockSizeInBytes
+	nBlocks := len(data) / BlockSize
 	for i := 0; i < nBlocks; i++ {
-		b := data[i*BlockSizeInBytes:]
+		b := data[i*BlockSize:]
 		c.toBlock(b)
 		// Used only 3 rounds
 		for r := uint32(3); r != 0; r-- {
 			var x uint32 = r * delta
 			rWord := c.block[0]
-			for k := blockSize - 1; k > 0; k-- {
+			for k := blockSizeInWords - 1; k > 0; k-- {
 				c.block[k] -= calcKey(c.block[k-1], rWord, x, uint32(k))
 				rWord = c.block[k]
 			}
-			c.block[0] -= calcKey(c.block[blockSize-1], rWord, x, uint32(0))
+			c.block[0] -= calcKey(c.block[blockSizeInWords-1], rWord, x, uint32(0))
 		}
 		c.fromBlock(b)
 	}
-	return nBlocks * BlockSizeInBytes
+	return nBlocks * BlockSize
 }
